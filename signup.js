51ed6fe079eb92b01497e1a1dedaa2081234bac8
Hasankhan170 <hasankhanali0316@gmail.com>
2024-07-30 02:40:35 -0700

    sign up complete

---
+++ signup.js
@@ -0,0 +1,36 @@
+import { createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import { auth } from "./config.js"
+
+const firstName = document.querySelector('.first-name')
+const signUpForm = document.querySelector('#signup-form')
+const lastName = document.querySelector('.last-name')
+const email = document.querySelector('.email')
+const password= document.querySelector('.password')
+const confirmPassword = document.querySelector('.confirm-password')
+
+signUpForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    if( email.value === '' || password.value === '' || confirmPassword.value === '' || firstName.value === '' || lastName.value === ''){
+        alert('please fill this')
+        return
+    }
+
+
+
+
+    createUserWithEmailAndPassword(auth, email.value, password.value,confirmPassword.value,firstName.value,lastName.value)
+  .then((userCredential) => {
+    
+    const user = userCredential.user;
+
+    console.log(user);
+
+    window.location = "./login.html"
+  })
+  .catch((error) => {
+    const errorMessage = error.message;
+    console.log(errorMessage);
+  });
+
+
+})
